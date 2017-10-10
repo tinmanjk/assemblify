54b81605064a4df1bf5ea60e14c896dfdd6e661c
tinmanjk <tinmanjk@mail.com>
2017-10-10 06:07:46 +0300

    added initial admin area files

---
+++ Assemblify.Web/Areas/Administration/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assemblify.Web.Areas.Administration.Controllers
+{
+    public class UsersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
