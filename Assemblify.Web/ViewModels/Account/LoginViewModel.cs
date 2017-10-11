c5dae5c2cd53d7c623b30185f1433449cea1348c
tinmanjk <tinmanjk@mail.com>
2017-10-11 07:30:33 +0300

    bugfix: cannot login - expected username to be equal to email address in SignInManager...

--- Assemblify.Web/ViewModels/Account/LoginViewModel.cs
+++ Assemblify.Web/ViewModels/Account/LoginViewModel.cs
@@ -6,9 +6,8 @@ namespace Assemblify.Web.ViewModels.Account
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
