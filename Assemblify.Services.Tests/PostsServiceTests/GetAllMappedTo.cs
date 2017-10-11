5815442ccb81135f8b87b50b9c2ead7f9eb998b1
tinmanjk <tinmanjk@mail.com>
2017-10-11 13:12:37 +0300

    added Test for GetAllMappedTo for valid returned object

--- Assemblify.Services.Tests/PostsServiceTests/GetAllMappedTo.cs
+++ Assemblify.Services.Tests/PostsServiceTests/GetAllMappedTo.cs
@@ -18,8 +18,17 @@ namespace Assemblify.Services.Tests.PostsServiceTests
     [TestFixture]
     public class GetAllMappedTo
     {
+        [SetUp]
+        public void SetupMapper()
+        {
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<Post, PostMapFrom>();
+            });
+        }
+
         [Test]
-        public void TestGetAll_ShouldCallRepositoryAll()
+        public void TestGetAllMappedTo_ShouldCallRepositoryAll()
         {
             // Arrange
             var mockedPostRepository = new Mock<IEfRepository<Post>>();
@@ -29,17 +38,44 @@ namespace Assemblify.Services.Tests.PostsServiceTests
             var service = new PostsService(mockedPostRepository.Object,
                 mockedSaveContext.Object);
 
-            Mapper.Initialize(config =>
-            {
-                config.CreateMap<Post, PostMapFrom>();
-            });
-
             // Act
-
             service.GetAllMappedTo<PostMapFrom>();
 
             // Assert
             mockedPostRepository.Verify(r => r.All, Times.Once);
+        }
+
+        [Test]
+        public void TestGetAllMappedTo_ShouldReturnMappedObject()
+        {
+            // Arrange
+            var mockedPostRepository = new Mock<IEfRepository<Post>>();
+            var mockedSaveContext = new Mock<ISaveContext>();
+            var mockedMapFromPost = new PostMapFrom();
+
+            var titleProperty = "zaglavie";
+
+            var postsMock = new List<Post>
+            {
+                new Post()
+                {
+                    Content="Interesting content",
+                    Title = titleProperty
+                }
+            };
+
+            mockedPostRepository.SetupGet(x => x.All).Returns(postsMock.AsQueryable);
+
+
+            var service = new PostsService(mockedPostRepository.Object,
+                mockedSaveContext.Object);
+
+            // Act
+
+            var result = service.GetAllMappedTo<PostMapFrom>();
+
+            // Assert
+            Assert.AreEqual(result.FirstOrDefault().Title, titleProperty);
         }
     }
 }
