01623d4864110c2f2261b4b85f9d167bad023a82
tinmanjk <tinmanjk@mail.com>
2017-10-11 12:49:32 +0300

    added unit test for GetAllMappedTo

---
+++ Assemblify.Services.Tests/PostsServiceTests/GetAllMappedTo.cs
@@ -0,0 +1,45 @@
+﻿using Assemblify.Data.Models;
+using Assemblify.Data.Repositories;
+using Assemblify.Data.SaveContext;
+using Assemblify.Infrastructure.Mapping;
+using Assemblify.Services.Tests.PostsServiceTests.Mocks;
+using Assemblify.Web.Controllers;
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemblify.Services.Tests.PostsServiceTests
+{
+    [TestFixture]
+    public class GetAllMappedTo
+    {
+        [Test]
+        public void TestGetAll_ShouldCallRepositoryAll()
+        {
+            // Arrange
+            var mockedPostRepository = new Mock<IEfRepository<Post>>();
+            var mockedSaveContext = new Mock<ISaveContext>();
+            var mockedMapFromPost = new PostMapFrom();
+
+            var service = new PostsService(mockedPostRepository.Object,
+                mockedSaveContext.Object);
+
+            Mapper.Initialize(config =>
+            {
+                config.CreateMap<Post, PostMapFrom>();
+            });
+
+            // Act
+
+            service.GetAllMappedTo<PostMapFrom>();
+
+            // Assert
+            mockedPostRepository.Verify(r => r.All, Times.Once);
+        }
+    }
+}
---
+++ Assemblify.Services.Tests/PostsServiceTests/Mocks/PostMapFrom.cs
@@ -0,0 +1,15 @@
+﻿using Assemblify.Data.Models;
+using Assemblify.Infrastructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemblify.Services.Tests.PostsServiceTests.Mocks
+{
+    public class PostMapFrom: IMapFrom<Post>
+    {
+        public string Title { get; set; }
+    }
+}
