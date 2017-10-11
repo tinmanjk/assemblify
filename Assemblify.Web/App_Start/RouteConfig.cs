744e8276b19c9f5d9d40d3ddd4284a7a9f65a694
tinmanjk <tinmanjk@mail.com>
2017-10-11 06:22:58 +0300

    added route debugger library

--- Assemblify.Web/App_Start/RouteConfig.cs
+++ Assemblify.Web/App_Start/RouteConfig.cs
@@ -20,6 +20,12 @@ namespace Assemblify.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "UserController",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
