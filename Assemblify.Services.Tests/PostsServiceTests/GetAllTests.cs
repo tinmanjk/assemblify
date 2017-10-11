eb7ff9312d65894adc86d07c6cd81b2e46d62067
tinmanjk <tinmanjk@mail.com>
2017-10-11 12:13:12 +0300

    added Service.Tests project with initial test

---
+++ Assemblify.Services.Tests/PostsServiceTests/GetAllTests.cs
@@ -0,0 +1,35 @@
+﻿using Assemblify.Data.Models;
+using Assemblify.Data.Repositories;
+using Assemblify.Data.SaveContext;
+using Assemblify.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemblify.Services.Tests.PostsServiceTests
+{
+    [TestFixture]
+    public class GetAllTests
+    {
+        [Test]
+        public void TestGetAll_ShouldCallRepositoryAll()
+        {
+            // Arrange
+            var mockedPostRepository = new Mock<IEfRepository<Post>>();
+            var mockedSaveContext = new Mock<ISaveContext>();
+
+            var service = new PostsService(mockedPostRepository.Object,
+                mockedSaveContext.Object);
+
+            // Act
+            service.GetAll();
+
+            // Assert
+            mockedPostRepository.Verify(r => r.All, Times.Once);
+        }
+    }
+}
