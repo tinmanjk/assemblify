f28441c5c36c69bf6d24d8a309a6865011852b73
tinmanjk <tinmanjk@mail.com>
2017-10-13 09:35:58 +0300

    added Controller and view for not found

---
+++ src/Assemblify.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assemblify.Web.Controllers
+{
+    public class ErrorController: Controller
+    {
+        public ActionResult NotFound()
+        {
+            return this.View();
+        }
+    }
+}
