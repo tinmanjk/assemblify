f8dcc2f554cb5a5e032d350e01f0518f43b62d45
tinmanjk <tinmanjk@mail.com>
2017-10-13 20:54:43 +0300

    fixed controller mapping conflict

--- src/Assemblify.Web/App_Start/RouteConfig.cs
+++ src/Assemblify.Web/App_Start/RouteConfig.cs
@@ -51,7 +51,8 @@ namespace Assemblify.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Assemblify.Web.Controllers" }
             );
         }
 
