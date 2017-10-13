1e60fa0a382217e738fe9c1889841fff80d26bf4
tinmanjk <tinmanjk@mail.com>
2017-10-13 14:22:55 +0300

    added initial new post logic

---
+++ src/Assemblify.Web/Constants/PartialViews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assemblify.Web.Constants
+{
+    public class PartialViews
+    {
+        public const string LoginPartial = "_LoginPartial";
+        public const string NewPostPartial = "_NewPostPartial";
+
+
+    }
+}
---
+++ src/Assemblify.Web/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assemblify.Web.Controllers
+{
+    public class PostController : Controller
+    {
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            //var model = this.factory.CreateCreateLogViewModel();
+
+            return this.View();
+        }
+    }
+}
