d5d3836b345658060e6e9e2e409380899d5dc6cc
tinmanjk <tinmanjk@mail.com>
2017-10-15 02:20:34 +0300

    added solution for converting server time to client time for DateTimes

---
+++ src/Assemblify.Web/Filters/ClientTimeZoneAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assemblify.Web.Filters
+{
+    public class ClientTimeZoneAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var cookie = filterContext.HttpContext.Request.Cookies["timeZoneOffset"];
+
+            Int16 n = 0;
+
+            var tzo = (cookie != null && Int16.TryParse(cookie.Value, out n) ? n : 0);
+
+            filterContext.HttpContext.Session["tzo"] = tzo;
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ src/Assemblify.Web/Helpers/ClientTimeZoneHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assemblify.Web.Helpers
+{
+    public class ClientTimeZoneHelper
+    {
+        public static string ConvertToLocalTimeAndFormat(DateTime? dt, string format)
+        {
+            if (dt == null)
+            {
+                return "";
+
+            }
+            var dtNonNull = (DateTime)dt;
+
+            var o = HttpContext.Current.Session["tzo"];
+
+            var tzo = o == null ? 0 : Convert.ToDouble(o);
+
+            dtNonNull = dtNonNull.AddMinutes(-1 * tzo);
+
+            var s = dtNonNull.ToString(format);
+
+            if (tzo == 0)
+                s += " GMT";
+
+            return s;
+        }
+    }
+}
