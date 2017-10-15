2e705b3fd2d3a5c3d5477b1375dc2454d513249e
tinmanjk <tinmanjk@mail.com>
2017-10-15 18:17:29 +0300

    added Constructor Tests for AuthenticationProvider

---
+++ src/Assemblify.Web.Providers.Tests/AuthenticationProviderTests/ConstructorTests.cs
@@ -0,0 +1,56 @@
+﻿using Assemblify.Infrastructure.Providers.Contracts;
+using Assemblify.Web.Providers.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using Microsoft.Owin;
+using System.Security.Principal;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assemblify.Web.Providers.Tests.AuthenticationProviderTests.Mocks;
+
+namespace Assemblify.Web.Providers.Tests.AuthenticationProviderTests
+{
+    [TestFixture]
+    public class ConstructorTests
+    {
+        [Test]
+        public void Constructor_WithPassedDateTimeProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedHttpContextProvider = new MockedHttpContextProvider();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MockedAuthenticationProvider(null, mockedHttpContextProvider));
+        }
+
+        [Test]
+        public void Constructor_WithPassedHttpContextProviderNull_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            // Act, Assert
+            Assert.Throws<ArgumentNullException>(() =>
+                new MockedAuthenticationProvider(mockedDateTimeProvider.Object, null));
+        }
+
+        [Test]
+        public void Constructor_ShouldInitializeCorrectly()
+        {
+            // Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            var mockedHttpContextProvider = new MockedHttpContextProvider();
+
+            // Act
+            var authenticationProvider = new AuthenticationProvider(mockedDateTimeProvider.Object, mockedHttpContextProvider);
+
+            // Assert
+            Assert.IsNotNull(authenticationProvider);
+        }
+    }
+}
---
+++ src/Assemblify.Web.Providers.Tests/AuthenticationProviderTests/Mocks/MockedAuthenticationProvider.cs
@@ -0,0 +1,29 @@
+﻿using Assemblify.Infrastructure.Providers.Contracts;
+using Assemblify.Web.Providers.Contracts;
+using Assemblify.Web.Providers.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assemblify.Web.Providers.Tests.AuthenticationProviderTests.Mocks
+{
+    public class MockedAuthenticationProvider : AuthenticationProvider
+    {
+        public MockedAuthenticationProvider(IDateTimeProvider dateTimeProvider, IHttpContextProvider httpContextProvider)
+            : base(dateTimeProvider, httpContextProvider)
+        {
+        }
+
+        public ApplicationSignInManager GetSignInManager()
+        {
+            return this.SignInManager;
+        }
+
+        public ApplicationUserManager GetUserManager()
+        {
+            return this.UserManager;
+        }
+    }
+}
