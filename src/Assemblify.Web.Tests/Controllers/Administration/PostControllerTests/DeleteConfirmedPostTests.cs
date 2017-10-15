6e076cde0de1e0a334412b52ab9accf5920a9ff1
tinmanjk <tinmanjk@mail.com>
2017-10-16 02:52:57 +0300

    added delete confirm test. finished administration-area controller testing

---
+++ src/Assemblify.Web.Tests/Controllers/Administration/PostControllerTests/DeleteConfirmedPostTests.cs
@@ -0,0 +1,38 @@
+﻿using Assemblify.Services.Contracts;
+using Assemblify.Web.Areas.Administration.Controllers;
+using Assemblify.Web.Providers.Contracts;
+using AutoMapper;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.FluentMVCTesting;
+
+namespace Assemblify.Web.Tests.Controllers.Administration.PostControllerTests
+{
+    [TestFixture]
+    public class DeleteConfirmedPostTests
+    {
+        [Test]
+        public void DeleteConfirmed_ShouldRedirectToIndex()
+        {
+            // Arrange
+            var mockedPostService = new Mock<IPostService>();
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedMapper = new Mock<IMapper>();
+
+            var controller = new PostController(mockedPostService.Object, mockedAuthenticationProvider.Object,
+               mockedMapper.Object);
+
+            Guid id = Guid.NewGuid();
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.DeleteConfirmed(id))
+                .ShouldRedirectTo((PostController pc) => pc.Index());
+        }
+    }
+}
