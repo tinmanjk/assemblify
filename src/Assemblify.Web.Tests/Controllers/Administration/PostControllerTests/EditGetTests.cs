34517e3320b8e13de2926cb9868fffada43c183e
tinmanjk <tinmanjk@mail.com>
2017-10-16 01:10:21 +0300

    added EditGet tests for null parameters

--- src/Assemblify.Web.Tests/Controllers/Administration/PostControllerTests/EditGetTests.cs
+++ src/Assemblify.Web.Tests/Controllers/Administration/PostControllerTests/EditGetTests.cs
@@ -9,8 +9,10 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using TestStack.FluentMVCTesting;
 
 namespace Assemblify.Web.Tests.Controllers.Administration.PostControllerTests
@@ -19,7 +21,50 @@ namespace Assemblify.Web.Tests.Controllers.Administration.PostControllerTests
     public class EditGetTests
     {
         [Test]
-        public void CreateGet_WithCorredIdAndReturnedPost_ShouldReturnCorrectViewWithModel()
+        public void EditGet_WithNullId_ShouldReturnCorrectHttpStatusCode()
+        {
+            // Arrange
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            Guid? postId = null;
+
+            var controller = new PostController(mockedPostService.Object, mockedAuthenticationProvider.Object,
+               mockedMapper.Object);
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.Edit(postId))
+                .ShouldGiveHttpStatus(HttpStatusCode.BadRequest);
+                
+        }
+
+        [Test]
+        public void EditGet_WithCorrectIdAndNoFoundPost_ShouldReturnHttpNotFound()
+        {
+            // Arrange
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            Guid postId = Guid.NewGuid();
+            Post post = null;
+            mockedPostService.Setup(x => x.GetPostByIdAndDeleted(postId))
+                .Returns(post);
+
+            var controller = new PostController(mockedPostService.Object, mockedAuthenticationProvider.Object,
+               mockedMapper.Object);
+
+            // Act, Assert
+            controller
+                .WithCallTo(c => c.Edit(postId))
+                .ValidateActionReturnType<HttpNotFoundResult>();
+
+        }
+
+        [Test]
+        public void EditGet_WithCorrectIdAndReturnedPost_ShouldReturnCorrectViewWithModel()
         {
             // Arrange
             var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
